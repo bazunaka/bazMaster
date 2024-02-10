b25d012aabf150cd0fc171dbf9ac0562a7fe9c62
bazunaka <40122285+bazunaka@users.noreply.github.com>
2024-02-10 20:05:32 +0300

    add new files

--- bazmasterapp/views.py
+++ bazmasterapp/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def main(request):
+    return render(request, 'main.html')
