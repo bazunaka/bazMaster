3a848ecc4ff7c89c5540ba5c2ad3b75253db7ed1
bazunaka-macbook <bazunaka32@gmail.com>
2024-03-07 12:45:23 +0300

    add models

--- bazmasterapp/models.py
+++ bazmasterapp/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=250, db_index=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
