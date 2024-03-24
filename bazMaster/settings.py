b7bf515753dc92679804efc8acdc4956b9d6973a
bazunaka <40122285+bazunaka@users.noreply.github.com>
2024-03-24 20:58:15 +0300

    add new connection

--- bazMaster/settings.py
+++ bazMaster/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'bazMaster.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bazMaster',
+        'USER': 'postgres',
+        'PASSWORD': 'bazunaka!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
