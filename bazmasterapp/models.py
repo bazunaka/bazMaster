83a98b996aad9dd1f2fb4218f8dfeb7db8391f8c
bazunaka <40122285+bazunaka@users.noreply.github.com>
2024-03-25 19:45:22 +0300

    add new models

--- bazmasterapp/models.py
+++ bazmasterapp/models.py
@@ -1,4 +1,19 @@
 from django.db import models
 
+
 class Bank(models.Model):
     name_bank = models.CharField(max_length=100, verbose_name="Название банка", unique=True)
+
+
+class TypeBankAccounts(models.Model):
+    name_type_bank_acc = models.CharField(max_length=100, verbose_name="Название типа счета", unique=True)
+
+
+class BankAccounts(models.Model):
+    id_bank = models.ForeignKey(Bank, on_delete=models.CASCADE, verbose_name="Название банка")
+    id_type = models.ForeignKey(TypeBankAccounts, on_delete=models.CASCADE, verbose_name="Название типа счета")
+    name_bank_acc = models.CharField(max_length=100, verbose_name="Название счета")
+    summary = models.FloatField(verbose_name="Сумма")
+    create_date = models.DateField(verbose_name="Дата создания")
+    close_date = models.DateField(verbose_name="Дата закрытия")
+    isActive = models.BooleanField(verbose_name="Активен")
