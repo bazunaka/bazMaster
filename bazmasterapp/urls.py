961f310e56557e542a59798fbf02f617bd74efdc
bazunaka-macbook <bazunaka32@gmail.com>
2024-03-28 12:04:12 +0300

    add routing

--- bazmasterapp/urls.py
+++ bazmasterapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('forgot-password', views.forgot_password),
-    path('register/', views.register),
+    path('forgot-password', views.forgot_password, name='forgot-password'),
+    path('register/', views.register, name='register'),
     path('login/', views.login),
     path('main/', views.main),
     path('', views.login),
